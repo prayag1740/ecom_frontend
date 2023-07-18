e610a2eced238c96b1e65dfab8ee87a0f7005d5d
Prayag Bhatia <prayag.bhatia2015@vit.ac.in>
2023-07-18 13:45:01 +0530

    setting up Header component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import React from 'react';
+import { Header } from './components/Header';
+import {BrowserRouter as Router} from "react-router-dom"
 
 function App() {
   return (
     <>
-
+    <Router>
+    <Header />
+    </Router>
     </>
   );
 }
